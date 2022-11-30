e9c6c7deaef9fc2545c660ac32c32ed85a687be1
Dao Huong Milo <dao.huong@thoughtworks.com>
2022-11-30 15:40:01 +0700

    trello-clone: init

---
+++ src/components/dataset.jsx
@@ -0,0 +1,6 @@
+export const tasks = [
+  { id: 1, title: "Init project", description: "init project" },
+  { id: 1, title: "Init project", description: "init project" },
+  { id: 1, title: "Init project", description: "init project" },
+];
+export const columns = [{ id: 1, title: "TO DO", listTask: [1, 2] }];
